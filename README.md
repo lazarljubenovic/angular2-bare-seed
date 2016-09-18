a1f551666c8353585be069f409fe6fec4ff8cdbe
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2016-09-18 19:44:32 +0200

    :tada:

---
+++ README.md
@@ -0,0 +1,1 @@
+# Almost bare Angular 2.0.0 seed
---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Bare Angular 2 seed</h1>'
+})
+export class AppComponent {
+}
